dfd38b9ee5ce1326aec2a6669d9a5961f7e78f7d
Yaroslav Lysenko <lysenko.yaroslav.kharkov@gmail.com>
2019-05-05 22:32:55 +0300

    add jenkins_install.sh

---
+++ jenkins_install.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+sudo yum update -y;
+sudo yum install mc -y;
+
+#Java
+sudo yum install java-1.8.0-openjdk -y;
+java -version;
+
+sudo yum update -y;
+#Jenkins Repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+sudo yum update -y;
+
+#Install jenkins
+sudo yum install jenkins -y;
